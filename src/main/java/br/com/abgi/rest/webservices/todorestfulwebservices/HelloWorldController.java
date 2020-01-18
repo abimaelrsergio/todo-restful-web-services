516065e1b980678d5438b29e5c66ff689f92b706
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-01-18 17:57:40 -0300

    Atualizacao

--- src/main/java/br/com/abgi/rest/webservices/todorestfulwebservices/HelloWorldController.java
+++ src/main/java/br/com/abgi/rest/webservices/todorestfulwebservices/HelloWorldController.java
@@ -1,9 +1,11 @@
 package br.com.abgi.rest.webservices.todorestfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="http://localhost:4200") // Permite que uma porta específica em um dado servidor tenha acesso
 @RestController
 public class HelloWorldController {
 
