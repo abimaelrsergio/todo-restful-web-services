21e3e5677683408fc8db680de5fae18f13467a2c
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-01-26 22:12:52 -0300

    Atualizacao

--- src/main/java/br/com/abgi/rest/webservices/todorestfulwebservices/todo/TodoResource.java
+++ src/main/java/br/com/abgi/rest/webservices/todorestfulwebservices/todo/TodoResource.java
@@ -3,14 +3,16 @@ package br.com.abgi.rest.webservices.todorestfulwebservices.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.abgi.rest.webservices.todorestfulwebservices.todo.service.TodoHardCodedService;
 
-@CrossOrigin(origins="http://localhost:4200") // Permite que uma porta específica em um dado servidor tenha acesso
+@CrossOrigin(origins = "http://localhost:4200") // Permite que uma porta específica em um dado servidor tenha acesso
 @RestController
 public class TodoResource {
 
@@ -20,5 +22,20 @@ public class TodoResource {
 	@GetMapping("/users/{username}/todos")
 	public List<Todo> getAllTodos(@PathVariable String username) {
 		return todoService.findAll();
+	}
+
+	@GetMapping("/users/{username}/todos/{id}")
+	public Todo getTodo(@PathVariable String username, @PathVariable long id) {
+		return todoService.findById(id);
+	}	
+	
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
+		Todo todo = todoService.deleteById(id);
+		if (todo != null) {
+			return ResponseEntity.noContent().build();	
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
--- src/main/java/br/com/abgi/rest/webservices/todorestfulwebservices/todo/service/TodoHardCodedService.java
+++ src/main/java/br/com/abgi/rest/webservices/todorestfulwebservices/todo/service/TodoHardCodedService.java
@@ -21,8 +21,41 @@ public class TodoHardCodedService {
 		todos.add(new Todo(++idCounter, "Guarda-costas", "Só pra ricos", new Date(), false));
 	}
 
+	public Todo save(Todo todo) {
+
+		if (todo.getId() == -1) {
+			todo.setId(++idCounter);
+			todos.add(todo);
+		} else {
+			deleteById(todo.getId());
+			todos.add(todo);
+		}
+		return todo;
+	}
+
 	public List<Todo> findAll() {
 		return todos;
+	}
+
+	public Todo deleteById(long id) {
+
+		Todo todo = findById(id);
+
+		if (todo == null)
+			return null;
+
+		if (todos.remove(todo))
+			return todo;
+
+		return null;
+	}
+
+	public Todo findById(long id) {
+		for (Todo todo : todos) {
+			if (todo.getId() == id)
+				return todo;
+		}
+		return null;
 	}
 
 }
