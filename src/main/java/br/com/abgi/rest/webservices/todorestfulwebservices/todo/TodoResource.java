72e20eb4dbdff2af6569a402945f17f2ab4e4a07
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-01-26 23:52:34 -0300

    Atualizacao

--- src/main/java/br/com/abgi/rest/webservices/todorestfulwebservices/todo/TodoResource.java
+++ src/main/java/br/com/abgi/rest/webservices/todorestfulwebservices/todo/TodoResource.java
@@ -1,14 +1,20 @@
 package br.com.abgi.rest.webservices.todorestfulwebservices.todo;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.abgi.rest.webservices.todorestfulwebservices.todo.service.TodoHardCodedService;
 
@@ -27,15 +33,38 @@ public class TodoResource {
 	@GetMapping("/users/{username}/todos/{id}")
 	public Todo getTodo(@PathVariable String username, @PathVariable long id) {
 		return todoService.findById(id);
-	}	
-	
+	}
+
 	@DeleteMapping("/users/{username}/todos/{id}")
 	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
 		Todo todo = todoService.deleteById(id);
 		if (todo != null) {
-			return ResponseEntity.noContent().build();	
+			return ResponseEntity.noContent().build();
 		}
-		
+
 		return ResponseEntity.notFound().build();
+	}
+
+	// Método para atualizar uma tarefa existente
+	@PutMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id,
+			@RequestBody Todo todo) {
+
+		Todo todoUpdated = todoService.save(todo);
+
+		return new ResponseEntity<Todo>(todoUpdated, HttpStatus.OK);
+
+	}
+
+	// Método para criar uma nota tarefa
+	@PostMapping("/users/{username}/todos")
+	public ResponseEntity<Void> saveTodo(@PathVariable String username, @RequestBody Todo todo){
+		
+		Todo createdTodo = todoService.save(todo);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
 	}
 }
